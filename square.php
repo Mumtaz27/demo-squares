ee52e598b55223ca7a5460135590d32f84b599e8
Mumtaz27 <94848161+Mumtaz27@users.noreply.github.com>
2021-11-22 11:22:48 -0500

    create square

---
+++ square.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+
+<h2>Square CSS</h2>
+.square {
+  display: block;
+  float: left;
+  border: 1px red;
+  width: 50px;
+  height: 50px;
+  margin: 30px;
+}
+<div>
+  <div class="square"></div>
+  <div class="square"></div>
+  <div class="square"></div>
+  <div class="square"></div>
+  <div class="square"></div>
+  <div class="square"></div>
+</div>
+
+<div>
+  <div class="square"></div>
+  <div class="square"></div>
+  <div class="square"></div>
+  <div class="square"></div>
+  <div class="square"></div>
+  <div class="square"></div>
+</div>
+</body>
+</html> 
+</html>
