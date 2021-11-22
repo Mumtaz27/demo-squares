75ab32210a9e2971255eec8bc80b4c344be7c8c0
Mumtaz27 <94848161+Mumtaz27@users.noreply.github.com>
2021-11-22 10:49:22 -0500

    intialition index.php

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+    <form action="/index_page.php">
+        <label for="X">X:</label><br>
+        <input type="text" id="X" name="X" value=""><br>
+        <label for="Y">Y:</label><br>
+        <input type="text" id="Y" name="Y" value=""><br><br>
+        <input type="Send" value="Send">
+   ?>
