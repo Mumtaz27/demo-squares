0e19a3491c0cc18fd15c573c23ed898419c52bae
Mumtaz27 <94848161+Mumtaz27@users.noreply.github.com>
2021-11-23 08:32:37 -0500

    demo-squares clone

--- index.php
+++ index.php
@@ -1,8 +1,12 @@
-<?php
-    <form action="/index_page.php">
+<!DOCTYPE html>
+<html lang = "en">
+     <head></head>
+     <body>
+   <form action="/index.php">
         <label for="X">X:</label><br>
         <input type="text" id="X" name="X" value=""><br>
         <label for="Y">Y:</label><br>
         <input type="text" id="Y" name="Y" value=""><br><br>
         <input type="Send" value="Send">
-   ?>
+        </form>
+</body>
